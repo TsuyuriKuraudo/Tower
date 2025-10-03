99a1ed24a03a53970bb6f3bdf672f1440817cdaf
TsuyuriKuraudo <hyt0_1_1_2_3_5_8@yahoo.com>
2025-10-03 21:41:27 +0800

    move brave

--- tower/brave.gd
+++ tower/brave.gd
@@ -5,5 +5,19 @@ var animation:AnimatedSprite2D
 func _ready() -> void:
 	animation = get_node("Animation")
 	#animation.play("front")
-	self.position.x = 16;
-	self.position.y = 16;
+	self.position.x = 16
+	self.position.y = 16
+	
+func  _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_left"):
+		self.position.x-=32
+		animation.play("left")
+	if Input.is_action_just_pressed("ui_right"):
+		self.position.x+=32
+		animation.play("right")
+	if Input.is_action_just_pressed("ui_up"):
+		self.position.y-=32
+		animation.play("back")
+	if Input.is_action_just_pressed("ui_down"):
+		self.position.y+=32
+		animation.play("front")
